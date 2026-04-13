577fd9b9018b8accc1f2265855cbf3b39515d362
MCota98 <manuelcota1307@gmail.com>
2026-04-13 17:29:22 -0600

    updaring

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills(),
-  ]
+    nodePolyfills({
+      // 🔥 EL SECRETO: Le prohibimos al polyfill que sobrescriba 'process'
+      globals: {
+        Buffer: true,
+        global: true,
+        process: false, 
+      },
+    }),
+  ],
+  define: {
+    // Ahora nuestra variable está blindada y nadie la va a borrar
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': JSON.stringify({ NODE_ENV: 'production' })
+  }
 })
