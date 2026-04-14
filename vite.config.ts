a4889d1ac1b151600e95b7e57ebc5206feb6c088
MCota98 <manuelcota1307@gmail.com>
2026-04-13 22:53:24 -0600

    updaring

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,10 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineConfig({
   plugins: [
     react(),
-    // Dejamos que el polyfill haga su trabajo completo sin restricciones
-    nodePolyfills(), 
+    nodePolyfills(),
   ],
   define: {
-    // Definimos la variable de producción estrictamente para Web3Modal
-    'process.env.NODE_ENV': '"production"',
+    'process.env': JSON.stringify({ NODE_ENV: 'production' }),
     global: 'globalThis'
-  },
-  optimizeDeps: {
-    // 🔥 LA ORDEN NUCLEAR: Fuerza a Vite a destruir la caché corrupta
-    force: true 
   }
 })
